7482d74e92d3bfe3939308757305dcbda989528d
newsty <xadichaermatova19@gmail.com>
2023-06-26 10:00:55 +0500

    objekt,array

---
+++ main.js
@@ -0,0 +1,87 @@
+//   Array
+// 1-Array
+let student = [
+  {
+    name: "Shahnoza",
+    age: "18",
+    years: "2005",
+    tel: "90 564 12 35",
+  },
+  {
+    name: "Nodira",
+    age: "20",
+    years: "2003",
+    tel: "93 456 12 78",
+  },
+  {
+    name: "Zebo",
+    age: "18",
+    years: "2005",
+    tel: "95 456 58 69",
+  },
+  {
+    name: "Muslima",
+    age: "19",
+    years: "2004",
+    tel: "90 456 87 12",
+  },
+];
+console.log(student);
+
+// 2-Array
+let phone = [
+  {
+    name: "Xiaomi Radmi A12",
+    sum: "1 352 000",
+    color: ["Black", "Blue", "Green"],
+  },
+  {
+    name: "Huawei",
+    sum: "12 992 000",
+    color: ["Gray", "Blue", "Green", "Black"],
+  },
+  {
+    name: "Samsung Galaxy A24",
+    sum: "3 092 000",
+    color: ["Gray", "Blue", "Black"],
+  },
+
+  "https://olcha.uz/oz/category/telefony-gadzhety-aksessuary/telefony?page=4",
+];
+console.log(phone[2]);
+
+// 3-Array
+const hospital = [
+  {
+    name: "Terapevt",
+    time: "8:00  14:00",
+    soum: "70 000 so'm",
+  },
+  {
+    name: "Lor",
+    time: "9:00  16:00",
+    soum: "100 000 so'm",
+  },
+  {
+    name: "Xirurg",
+    time: "8:00  14:00",
+    soum: "120 000 so'm",
+  },
+  {
+    name: "Stomotolog",
+    time: "12:00  19:00",
+    soum: "80 000 so'm",
+  },
+  "Manzil:Maxtumquli ko'chasi 105, Yashnobod tumani, Toshkent",
+  ["+998-78-129-18-18", "+998-97-772-52-03"],
+];
+console.log(hospital[4]);
+
+// Obyekt
+let kitob = {
+  nomi: "Tafsiri Hilol",
+  muallif: "Shayx Muhammad Sodiq Muhammad Yusuf",
+  nashriyot: "Hilol Nashr",
+  sum: "10300",
+  juz: [1, 2, 3, 4, 5],
+};
